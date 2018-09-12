0f995937b1a44cbe765930c2249aff9a4daee71d
PatsakornBoom <42571318+PatsakornBoom@users.noreply.github.com>
2018-09-12 11:16:13 +0700

    Initial commit

---
+++ OpenCV 3TrackingAPI/OpenCV3TrackingAPI.cpp
@@ -0,0 +1,144 @@
+﻿#include "opencv2/opencv.hpp"
+#include "opencv2/core/ocl.hpp"
+#include "opencv2/video.hpp"
+#include <tracking/tracker.hpp>
+/*#include <opencv2/core/utility.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"*/
+using namespace cv;
+using namespace std;
+
+
+/*void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_RBUTTONDOWN)
+	{
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_MBUTTONDOWN)
+	{
+		cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+
+	}
+}*/
+
+// Convert to string
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+( std::ostringstream() << std::dec << x ) ).str()
+
+int main(int argc, char **argv)
+{
+	// List of tracker types in OpenCV 3.4.1
+	string trackerTypes[8] = { "BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT" };
+	// vector <string> trackerTypes(types, std::end(types));
+
+	// Create a tracker
+	string trackerType = trackerTypes[2];
+	Ptr<Tracker> tracker;
+	//Ptr<Tracker> tracker;
+
+#if (CV_MINOR_VERSION < 3)
+	{
+		tracker = Tracker::create(trackerType);
+	}
+#else //(CV_MINOR_VERSION >= 3)
+	{
+		if (trackerType == "BOOSTING")
+			tracker = TrackerBoosting::create();
+		if (trackerType == "MIL")
+			tracker = TrackerMIL::create();
+		if (trackerType == "KCF")
+			tracker = TrackerKCF::create();
+		if (trackerType == "TLD")
+			tracker = TrackerTLD::create();
+		if (trackerType == "MEDIANFLOW")
+			tracker = TrackerMedianFlow::create();
+		if (trackerType == "GOTURN")
+			tracker = TrackerGOTURN::create();
+		if (trackerType == "MOSSE")
+			tracker = TrackerMOSSE::create();
+		if (trackerType == "CSRT")
+			tracker = TrackerCSRT::create();
+	}
+#endif
+	// Read video
+	VideoCapture video("Chaplin.mp4");
+
+	// Exit if video is not opened
+	if (!video.isOpened())
+	{
+		cout << "Could not read video file" << endl;
+		system("pause");
+		return 1;
+	}
+	
+	// Read first frame 
+	Mat frame;
+	bool ok = video.read(frame);
+
+	// Define initial bounding box 
+	Rect2d bbox(520, 120, 110, 160);
+
+	// Uncomment the line below to select a different bounding box 
+	//bbox = selectROI(frame, false); 
+	//Display bounding box. 
+	rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+
+	/*while (1) {
+		imshow("Tracking", frame);
+		waitKey(1);
+	}*/
+	//setMouseCallback("Tracking", CallBackFunc, NULL);
+	imshow("Tracking", frame);
+	tracker->init(frame, bbox);
+
+	while (video.read(frame))
+	{
+		// Start timer
+		double timer = (double)getTickCount();
+
+		// Update the tracking result
+		bool ok = tracker->update(frame, bbox);
+
+		// Calculate Frames per second (FPS)
+		float fps = getTickFrequency() / ((double)getTickCount() - timer);
+
+		if (ok)
+		{
+			// Tracking success : Draw the tracked object
+			rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+		}
+		else
+		{
+			// Tracking failure detected.
+			putText(frame, "Tracking failure detected", Point(100, 80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2);
+		}
+
+		// Display tracker type on frame
+		putText(frame, trackerType + " Tracker", Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+		// Display FPS on frame
+		putText(frame, "FPS : " + std::to_string(int(fps)), Point(100, 50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
+
+		// Display frame.
+		imshow("Tracking", frame);
+
+		// Exit if ESC pressed.
+		int k = waitKey(1);
+		if (k == 27)
+		{
+			break;
+		}
+
+	}
+}
